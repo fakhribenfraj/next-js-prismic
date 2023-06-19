c17a66bb585adf741bd22b4b168bfc0d6279e500
fakhribenfraj <benfrajfakhreddine15@gmail.com>
2023-06-19 12:38:44 +0100

    add app directory nextjs

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+import { PrismicPreview } from "@prismicio/next";
+
+import { repositoryName } from "../prismicio";
+
+import "../styles/globals.css";
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="overflow-x-hidden antialiased">
+        {/* TODO: Remove the following element once you have read the documentation. */}
+        {process.env.NODE_ENV === "development" && (
+          <div
+            style={{
+              background: "#5163ba",
+              padding: "1rem",
+              textAlign: "center",
+              fontSize: "0.85rem",
+              color: "#fff",
+            }}
+          >
+            <p>
+              <strong>👋 Welcome to your new website!</strong> To customize the
+              code and content of this site,{" "}
+              <a
+                href="https://github.com/prismicio-community/nextjs-starter-prismic-blog/tree/master/docs"
+                target="_blank"
+                rel="noreferrer"
+                style={{ textDecoration: "underline" }}
+              >
+                see the documentation
+              </a>
+              . Remove this bar in <code>pages/_app.js</code>.
+            </p>
+          </div>
+        )}
+        {children}
+        <PrismicPreview repositoryName={repositoryName} />
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,48 @@
+// src/app/page.tsx
+
+import { asText } from "@prismicio/client";
+import { createClient } from "@/prismicio";
+import { Layout } from "@/components/Layout";
+import { Bounded } from "@/components/Bounded";
+import { Article } from "@/components/Article";
+
+
+export async function generateMetadata() {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
+  return {
+    title: asText(settings.data.title),
+  };
+}
+
+export default async function Home() {
+  const client = createClient();
+  const navigation = await client.getSingle("navigation");
+  const settings = await client.getSingle("settings");
+  const articles = await client.getAllByType("article", {
+    orderings: [
+      { field: "my.article.publishDate", direction: "desc" },
+      { field: "document.first_publication_date", direction: "desc" },
+    ],
+  });
+
+  return (
+    <Layout
+      withHeaderDivider={false}
+      navigation={navigation}
+      settings={settings}
+    >
+      {/* <Head>
+        <title>{prismic.asText(settings.data.name)}</title>
+      </Head> */}
+      <Bounded size="widest">
+        <ul className="grid grid-cols-1 gap-16">
+          {articles.map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
+        </ul>
+      </Bounded>
+    </Layout>
+  );
+}
